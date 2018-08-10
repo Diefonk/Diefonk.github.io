106e6063926035b62468c5016329b1fd4c53cb2a
Diefonk <thediefonk@gmail.com>
2018-08-10 17:49:47 +0200

    Added directories and files Also some cleanup in console.js

--- console.js
+++ console.js
@@ -1,115 +1,161 @@
-function onLoad() {
-	printString("diefonk.github.io");
-	document.getElementById("prompt").innerHTML = prompt;
+var prompt;
+var currentDirectory;
+
+function init() {
+	var code = new Directory("code");
+	code.addFile("https://github.com/Diefonk/processing-projects", "Processing Projects.pde");
+	code.addFile("https://github.com/Diefonk/baebot", "BaeBot.js");
+	code.addFile("https://github.com/Diefonk/console-website", "Console Website.js");
+	code.makeList();
+
+	var games = new Directory("games");
+	games.addFile("https://diefonk.itch.io/pizza-dress-up", "Pizza Dress-Up.js");
+	games.addFile("https://drive.google.com/drive/folders/1ko7IG3FnUxR1QBOHg39d2YUts0BJroTy", "The Last Delivery.love");
+	games.addFile("https://drive.google.com/drive/folders/1ko7IG3FnUxR1QBOHg39d2YUts0BJroTy", "Perkele Saatana.exe");
+	games.addFile("https://drive.google.com/drive/folders/1ko7IG3FnUxR1QBOHg39d2YUts0BJroTy", "Scout Mountain.exe");
+	games.addFile("https://drive.google.com/drive/folders/1ko7IG3FnUxR1QBOHg39d2YUts0BJroTy", "The Legend of Yuki.exe");
+	games.makeList();
+
+	currentDirectory = new Directory("index");
+	currentDirectory.addFile("https://twitter.com/Diefonk", "Twitter");
+	currentDirectory.addFile("https://www.youtube.com/user/Diefonk", "YouTube");
+	currentDirectory.addFile("https://diefonk-wip.tumblr.com/", "Tumblr");
+	currentDirectory.addFile("https://diefonk.itch.io/", "itch.io");
+	currentDirectory.addFile("https://github.com/Diefonk", "GitHub");
+	currentDirectory.addFile("https://soundcloud.com/diefonk", "SoundCloud");
+	currentDirectory.addDirectory(code);
+	currentDirectory.addDirectory(games);
+	currentDirectory.makeList();
+
+	print("Welcome to diefonk.github.io\nType 'help' for a list of commands");
+	setPrompt();
 	focusInput();
 }
 
 function handleInput(aEvent) {
 	if (aEvent.keyCode === 13) {
 		aEvent.preventDefault();
-		printString(prompt + document.getElementById("input").value);
-		switch (document.getElementById("input").value.toLowerCase()) {
-			case "help":
-				printString("How can I help you if I can't even help myself?");
-				break;
-			case "clear":
-				location.reload();
-				break;
-			case "make me a sandwich":
-				printString("What? Make it yourself.");
-				break;
-			case "sudo make me a sandwich":
-				printString("Okay.");
-				break;
-			case "xyzzy":
-				printString("Nothing happens");
-				break;
-			case "send nudes":
-				printString("No.");
-				break;
-			case "send noots":
-				printString("NOOT NOOT");
-				break;
-			case "about":
-				printRandom(about);
-				break;
-			case "":
-				break;
-			default:
-				printString("Command not found");
-				break;
+		print(prompt + document.getElementById("input").value);
+		const input = document.getElementById("input").value.toLowerCase();
+		if (input === "help") {
+			print("about - returns some information about Diefonk");
+			print("cd - changes directory to specified directory");
+			print("ls - lists directories and files in current directory");
+			print("open - opens the specified file");
+			print("clear - clears the console");
+			print("help - returns this list");
+		} else if (input.substring(0, 3) === "cd " || input === "cd") {
+			if (input.length < 4) {
+				print("No directory specified");
+			} else {
+				const path = input.substring(3, input.length);
+				var newDirectory = currentDirectory.getDirectory(path);
+				if (newDirectory !== null) {
+					currentDirectory = newDirectory;
+					setPrompt();
+				} else {
+					print(path + ": No such directory");
+				}
+			}
+		} else if (input === "ls") {
+			print(currentDirectory.getList());
+		} else if (input.substring(0, 5) === "open " || input === "open") {
+			if (input.length < 6) {
+				print("No file specified");
+			} else {
+				const name = input.substring(5, input.length);
+				var file = currentDirectory.getFile(name);
+				if (typeof(file) === "string") {
+					window.open(file, "_blank");
+				} else {
+					print(name + ": No such file");
+				}
+			}
+		} else if (input === "about") {
+			print("Diefonk " + about[random(about.length)]);
+		} else if (input === "clear") {
+			location.reload();
+		} else if (input === "xyzzy") {
+			print("Nothing happens");
+		} else if (input === "make me a sandwich") {
+			print("What? Make it yourself.");
+		} else if (input === "sudo make me a sandwich") {
+			print("Okay.");
+		} else if (input === "send nudes") {
+			print("No.");
+		} else if (input === "send noots") {
+			print("NOOT NOOT");
+		} else if (input === "") {
+			//do nothing
+		} else {
+			print("Command not found\nType 'help' for a list of commands");
 		}
 		document.getElementById("input").value = "";
 		window.scrollTo(0, document.body.scrollHeight);
 	}
 }
 
-function printString(aString) {
+function print(aString) {
 	var output = document.createElement("p");
-	var text = document.createTextNode(aString);
-	output.appendChild(text);
-	document.getElementById("output").appendChild(output);
-}
-
-function printArray(aArray) {
-	var output = document.createElement("p");
-	var text = document.createTextNode(aArray[0]);
-	output.appendChild(text);
-	for (var index = 1; index < aArray.length; index++) {
-		var newLine = document.createElement("br");
-		output.appendChild(newLine);
-		text = document.createTextNode(aArray[index]);
+	var text;
+	var newlineIndex = aString.indexOf("\n");
+	while (newlineIndex >= 0) {
+		text = document.createTextNode(aString.substring(0, newlineIndex));
 		output.appendChild(text);
+		var newline = document.createElement("br");
+		output.appendChild(newline);
+		aString = aString.substring(newlineIndex + 1, aString.length);
+		newlineIndex = aString.indexOf("\n");
 	}
+	text = document.createTextNode(aString);
+	output.appendChild(text);
 	document.getElementById("output").appendChild(output);
 }
 
-function printRandom(aArray) {
-	var index = Math.floor(Math.random() * aArray.length);
-	var output = document.createElement("p");
-	var text = document.createTextNode(aArray[index]);
-	output.appendChild(text);
-	document.getElementById("output").appendChild(output);
+function random(aMax) {
+	return Math.floor(Math.random() * aMax);
 }
 
 function focusInput() {
 	document.getElementById("input").focus();
 }
 
-var prompt = "cmd> ";
+function setPrompt() {
+	prompt = "guest:" + currentDirectory.getName() + "$ ";
+	document.getElementById("prompt").innerHTML = prompt;
+}
 
 var about = [
-	"Diefonk is tired",
-	"Diefonk is a rogue wall enthusiast",
-	"Diefonk makes things",
-	"Diefonk is a boyish girl making games about girly boys",
-	"Diefonk is a girly boy writing stories about boyish girls",
-	"Diefonk makes games",
-	"Diefonk writes stories",
-	"Diefonk plays music",
-	"Diefonk is not here right now. Can I take a message?",
-	"Diefonk wants to be everything at once",
-	"Diefonk is just a dreamer",
-	"Diefonk is my name, and making games is my game.",
-	"Diefonk is probably watching Netflix right now",
-	"Diefonk is probably playing Tetris right now",
-	"Diefonk is procrastinating",
-	"Diefonk watches the watchmen",
-	"Diefonk has a lot of stuff to do",
-	"Diefonk coded this website",
-	"Diefonk is a figment",
-	"Diefonk would like to inform you that you just lost The Game",
-	"Diefonk doesn’t have time for this",
-	"Diefonk is probably reading comics right now",
-	"Diefonk is probably sleeping right now",
-	"Diefonk is probably not sleeping right now",
-	"Diefonk is probably just thinking about stuff and things right now",
-	"Diefonk is probably watching anime right now",
-	"Diefonk is loading, please wait…",
-	"Diefonk is a fan of wet circles",
-	"Diefonk should be sleeping",
-	"Diefonk once stayed in bed for six hours after waking up",
-	"Diefonk is trying",
-	"Diefonk learned HTML and CSS on Codecademy",
-	"Diefonk is a rainbow aficionado"
+	"is tired",
+	"is a rogue wall enthusiast",
+	"makes things",
+	"is a boyish girl making games about girly boys",
+	"is a girly boy writing stories about boyish girls",
+	"makes games",
+	"writes stories",
+	"plays music",
+	"is not here right now. Can I take a message?",
+	"wants to be everything at once",
+	"is just a dreamer",
+	"is my name, and making games is my game.",
+	"is probably watching Netflix right now",
+	"is probably playing Tetris right now",
+	"is procrastinating",
+	"watches the watchmen",
+	"has a lot of stuff to do",
+	"coded this website",
+	"is a figment",
+	"would like to inform you that you just lost The Game",
+	"doesn’t have time for this",
+	"is probably reading comics right now",
+	"is probably sleeping right now",
+	"is probably not sleeping right now",
+	"is probably just thinking about stuff and things right now",
+	"is probably watching anime right now",
+	"is loading, please wait…",
+	"is a fan of wet circles",
+	"should be sleeping",
+	"once stayed in bed for six hours after waking up",
+	"is trying",
+	"is a rainbow aficionado"
 ];
