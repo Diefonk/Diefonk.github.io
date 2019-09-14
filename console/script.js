a3b9493aeddf8898ba9a69b1ec4b4074e9cdc93e
Diefonk <thediefonk@gmail.com>
2019-09-14 19:29:43 +0200

    Chnages to match console website project Restructuring Better support for smaller screens

--- console/script.js
+++ console/script.js
@@ -1,127 +1,129 @@
-var prompt;
-var currentDirectory;
+const welcomeMessage = "Welcome to diefonk.net\nType 'help' for a list of commands";
 
-function init() {
-	var code = new Directory("code");
-	code.addFile("https://github.com/Diefonk/processing-projects", "Processing Projects.pde");
-	code.addFile("https://github.com/Diefonk/baebot", "BaeBot.js");
-	code.addFile("https://github.com/Diefonk/console-website", "Console Website.js");
-	code.makeList();
+//directories and files
+var code = new Directory("code");
+code.addFile("https://github.com/Diefonk/processing-projects", "Processing Projects.pde");
+code.addFile("https://github.com/Diefonk/baebot", "BaeBot.js");
+code.addFile("https://github.com/Diefonk/console-website", "Console Website.js");
+code.makeList();
 
-	var games = new Directory("games");
-	games.addFile("https://diefonk.itch.io/pizza-dress-up", "Pizza Dress-Up.js");
-	games.addFile("https://diefonk.itch.io/potato-simulator-2014-remix", "Potato Simulator 2014 Remix.js");
-	games.addFile("https://diefonk.itch.io/9in1", "9in1.js");
-	games.addFile("https://diefonk.itch.io/beast-city-dreamers", "Beast City Dreamers.bitsy");
-	games.makeList();
+var games = new Directory("games");
+games.addFile("https://diefonk.itch.io/pizza-dress-up", "Pizza Dress-Up.js");
+games.addFile("https://diefonk.itch.io/potato-simulator-2014-remix", "Potato Simulator 2014 Remix.js");
+games.addFile("https://diefonk.itch.io/9in1", "9in1.js");
+games.addFile("https://diefonk.itch.io/beast-city-dreamers", "Beast City Dreamers.bitsy");
+games.makeList();
 
-	currentDirectory = new Directory("index");
-	currentDirectory.addFile("https://twitter.com/Diefonk", "Twitter.url");
-	currentDirectory.addFile("https://www.youtube.com/user/Diefonk", "YouTube.url");
-	currentDirectory.addFile("https://blog.diefonk.net/", "Tumblr.url");
-	currentDirectory.addFile("https://diefonk.itch.io/", "itch.io.url");
-	currentDirectory.addFile("https://github.com/Diefonk", "GitHub.url");
-	currentDirectory.addFile("https://soundcloud.com/diefonk", "SoundCloud.url");
-	currentDirectory.addDirectory(code);
-	currentDirectory.addDirectory(games);
-	currentDirectory.makeList();
+currentDirectory = new Directory("index");
+currentDirectory.addFile("https://twitter.com/Diefonk", "Twitter.url");
+currentDirectory.addFile("https://www.youtube.com/user/Diefonk", "YouTube.url");
+currentDirectory.addFile("https://blog.diefonk.net/", "Tumblr.url");
+currentDirectory.addFile("https://diefonk.itch.io/", "itch.io.url");
+currentDirectory.addFile("https://github.com/Diefonk", "GitHub.url");
+currentDirectory.addFile("https://soundcloud.com/diefonk", "SoundCloud.url");
+currentDirectory.addDirectory(code);
+currentDirectory.addDirectory(games);
+currentDirectory.makeList();
 
-	print("Welcome to diefonk.net\nType 'help' for a list of commands");
-	setPrompt();
-	focusInput();
-}
+//commands
+createCommand("about", "returns some information about Diefonk", aInput => {
+	if (aInput !== "about") {
+		return false;
+	}
+	print("Diefonk " + about[random(about.length)]);
+	return true;
+});
 
-function handleInput(aEvent) {
-	if (aEvent.keyCode === 13) {
-		aEvent.preventDefault();
-
-		print(prompt + document.getElementById("input").value);
-		const input = document.getElementById("input").value.toLowerCase();
-		document.getElementById("input").value = "";
-
-		if (input === "help") {
-			print("about - returns some information about Diefonk");
-			print("cd - changes directory to specified directory");
-			print("ls - lists directories and files in current directory");
-			print("open - opens the specified file");
-			print("clear - clears the console");
-			print("help - returns this list");
-		} else if (input.substring(0, 3) === "cd " || input === "cd") {
-			if (input.length < 4) {
-				print("No directory specified");
-			} else {
-				const path = input.substring(3, input.length);
-				var newDirectory = currentDirectory.getDirectory(path);
-				if (newDirectory !== null) {
-					currentDirectory = newDirectory;
-					setPrompt();
-				} else {
-					print(path + ": No such directory");
-				}
-			}
-		} else if (input === "ls") {
-			print(currentDirectory.getList());
-		} else if (input.substring(0, 5) === "open " || input === "open") {
-			if (input.length < 6) {
-				print("No file specified");
-			} else {
-				const name = input.substring(5, input.length);
-				var file = currentDirectory.getFile(name);
-				if (typeof(file) === "string") {
-					window.open(file, "_blank");
-				} else {
-					print(name + ": No such file");
-				}
-			}
-		} else if (input === "about") {
-			print("Diefonk " + about[random(about.length)]);
-		} else if (input === "clear") {
-			location.reload();
-		} else if (input === "xyzzy") {
-			print("Nothing happens");
-		} else if (input === "make me a sandwich") {
-			print("What? Make it yourself.");
-		} else if (input === "sudo make me a sandwich") {
-			print("Okay.");
-		} else if (input === "send nudes") {
-			print("No.");
-		} else if (input === "send noots") {
-			print("NOOT NOOT");
-		} else if (input === "") {
-			//do nothing
+createCommand("cd", "changes directory to specified directory", aInput => {
+	if (aInput.length < 4) {
+		print("No directory specified");
+	} else {
+		const path = aInput.substring(3, aInput.length);
+		var newDirectory = currentDirectory.getDirectory(path);
+		if (newDirectory !== null) {
+			currentDirectory = newDirectory;
+			setPrompt();
 		} else {
-			print("Command not found\nType 'help' for a list of commands");
+			print(path + ": No such directory");
 		}
-		window.scrollTo(0, document.body.scrollHeight);
 	}
-}
+	return true;
+});
 
-function print(aString) {
-	var output = document.createElement("p");
-	var text;
-	var newlineIndex = aString.indexOf("\n");
-	while (newlineIndex >= 0) {
-		text = document.createTextNode(aString.substring(0, newlineIndex));
-		output.appendChild(text);
-		var newline = document.createElement("br");
-		output.appendChild(newline);
-		aString = aString.substring(newlineIndex + 1, aString.length);
-		newlineIndex = aString.indexOf("\n");
+createCommand("ls", "lists directories and files in current directory", aInput => {
+	if (aInput !== "ls" && aInput !== "dir") {
+		return false;
 	}
-	text = document.createTextNode(aString);
-	output.appendChild(text);
-	document.getElementById("output").appendChild(output);
-}
+	print(currentDirectory.getList());
+	return true;
+});
 
-function random(aMax) {
-	return Math.floor(Math.random() * aMax);
-}
+createCommand("dir", null, commands.ls);
 
-function focusInput() {
-	document.getElementById("input").focus();
-}
+createCommand("open", "opens the specified file", aInput => {
+	if (aInput.length < 6) {
+		print("No file specified");
+	} else {
+		const name = aInput.substring(5, aInput.length);
+		var file = currentDirectory.getFile(name);
+		if (typeof(file) === "string") {
+			window.open(file, "_blank");
+		} else {
+			print(name + ": No such file");
+		}
+	}
+	return true;
+});
 
-function setPrompt() {
-	prompt = "guest:" + currentDirectory.getName() + "$ ";
-	document.getElementById("prompt").innerHTML = prompt;
-}
+createCommand("clear", "clears the console", aInput => {
+	if (aInput !== "clear") {
+		return false;
+	}
+	location.reload();
+	return true;
+});
+
+createCommand("xyzzy", null, aInput => {
+	if (aInput !== "xyzzy") {
+		return false;
+	}
+	print("Nothing happens");
+	return true;
+});
+
+createCommand("make", null, aInput => {
+	if (aInput !== "make me a sandwich") {
+		return false;
+	}
+	print("What? Make it yourself.");
+	return true;
+});
+
+createCommand("sudo", null, aInput => {
+	if (aInput !== "sudo make me a sandwich") {
+		return false;
+	}
+	print("Okay.");
+	return true;
+});
+
+createCommand("send", null, aInput => {
+	if (aInput === "send nudes") {
+		print("No.");
+		return true;
+	} else if (aInput === "send noots") {
+		print("NOOT NOOT");
+		return true;
+	}
+	return false;
+});
+
+createCommand("help", "returns this list", aInput => {
+	if (aInput !== "help") {
+		return false;
+	}
+	for (let index = 0; index < commandList.length; index++) {
+		print(commandList[index]);
+	}
+	return true;
+});
