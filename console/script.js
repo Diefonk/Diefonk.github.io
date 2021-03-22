ade0c6adaf92ab8c815a171c70dcdc01dadbe009
Diefonk <thediefonk@gmail.com>
2021-03-22 14:11:09 +0100

    Removed Imgur link

--- console/script.js
+++ console/script.js
@@ -37,7 +37,6 @@ currentDirectory.addFile("https://blog.diefonk.net/", "tumblr.url");
 currentDirectory.addFile("https://diefonk.itch.io/", "itch.io.url");
 currentDirectory.addFile("https://github.com/Diefonk", "github.url");
 currentDirectory.addFile("https://soundcloud.com/diefonk", "soundcloud.url");
-currentDirectory.addFile("https://imgur.com/user/Diefonk", "imgur.url");
 currentDirectory.addDirectory(games);
 currentDirectory.addDirectory(other);
 currentDirectory.addDirectory(lists);
